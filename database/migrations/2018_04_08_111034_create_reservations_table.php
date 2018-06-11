8352c06c740aaac922cba61f1e491cbd411439da
dekutha <kutha@marketbiz.net>
2018-06-11 11:39:56 +0800

    initial commit

---
+++ database/migrations/2018_04_08_111034_create_reservations_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReservationsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('reservation_code');
+            $table->string('room');
+            $table->string('customer');
+            $table->string('checkin_date');
+            $table->string('checkout_date');
+            $table->string('status');
+            $table->string('reservation_date');
+            $table->string('employee_incharge');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reservations');
+    }
+}
